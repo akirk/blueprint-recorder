2e4576625de2ef94f9eca03f05d930d8d4a278c9
Alex Kirk <akirk@users.noreply.github.com>
2024-07-16 15:26:47 +0200

    Log SQL queries and expose them in the blueprint

--- expose-blueprint.php
+++ expose-blueprint.php
@@ -14,11 +14,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use WP_REST_Controller;
 use WP_REST_Server;
+use WP_Query;
 
 class BlueprintGenerator {
+	private $is_logging = false;
 	public function __construct() {
 		add_action( 'rest_api_init', array( $this, 'register_playground_blueprint_endpoint' ) );
 		add_action( 'rest_api_init', array( $this, 'add_cors_support' ) );
+		add_action( 'init', array( $this, 'init' ) );
+		add_filter( 'query', array( $this, 'log_query' ) );
+		add_action( 'admin_menu', array( $this, 'add_admin_menu' ) );
+		add_action( 'admin_post_clear_sql_logs', array( $this, 'clear_sql_logs' ) );
 	}
 
 	public function register_playground_blueprint_endpoint() {
@@ -160,17 +166,40 @@ class BlueprintGenerator {
 				'path' => 'wordpress/wp-content/mu-plugins',
 
 			);
-			$data = '<?php add_action(\'admin_notices\', function() {';
-			$data .= 'echo \'<div class="notice notice-error is-dismissible" id="expose-blueprint-plugin-message"><p><strong>The following plugins were not loaded since they are not available in the WordPress.org plugin directory:</strong></p><ul>';
+			$data = '<' . '?php add_action("admin_notices", function() {';
+			$data .= 'echo "<div class="notice notice-error is-dismissible" id="expose-blueprint-plugin-message"><p><strong>The following plugins were not loaded since they are not available in the WordPress.org plugin directory:</strong></p><ul>';
 			foreach ( $ignored_plugins as $plugin ) {
 				$data .= '<li>' . esc_html( $plugin ) . '</li>';
 			}
-			$data .= '</ul></div>\';';
+			$data .= '</ul></div>";';
 			$data .= '});';
 			$steps[] = array(
 				'step' => 'writeFile',
 				'path' => 'wordpress/wp-content/mu-plugins/expose-blueprint.php',
 				'data' => $data,
+			);
+		}
+
+		$args = array(
+			'post_type' => 'sql_log',
+			'posts_per_page' => -1
+		);
+
+		$query = new WP_Query( $args );
+
+		$sql_logs = array();
+		if ( $query->have_posts() ) {
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				$sql_logs[] = get_the_content() . ";\n";
+			}
+		}
+
+		wp_reset_postdata();
+		if ( ! empty( $sql_logs ) ) {
+			$steps[] = array(
+				'step' => 'runSql',
+				'sql' => $sql_logs,
 			);
 		}
 
@@ -188,8 +217,90 @@ class BlueprintGenerator {
 			'steps'               => $steps,
 		);
 
-		return new \WP_REST_Response( $blueprint, 200 );
+		return $blueprint;
 	}
+
+	public function add_admin_menu() {
+		add_menu_page(
+			'Blueprint',       // Page title
+			'Blueprint',       // Menu title
+			'manage_options',         // Capability
+			'blueprint',       // Menu slug
+			array( $this, 'render_admin_page' ), // Callback
+			'dashicons-list-view'     // Icon
+		);
+	}
+
+	public function render_admin_page() {
+		$blueprint = $this->generate_playground_blueprint();
+
+		echo '<div class="wrap">';
+		echo '<h1>Blueprint</h1>';
+		echo '<a href="https://playground.wordpress.net/#' . rawurlencode( json_encode( $blueprint ) ) . '" target="_blank">Start Playground with the blueprint below</a><br/>';
+		echo '<textarea cols="120" rows="200">' . esc_html( json_encode( $blueprint, JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT ) )  . '</textarea>';
+
+	}
+
+	public function init() {
+		// Register custom post type for logs
+		register_post_type( 'sql_log', [
+			'labels' => [
+				'name' => 'SQL Logs',
+				'singular_name' => 'SQL Log',
+			],
+			'public' => false,
+			'show_ui' => true,
+			'capability_type' => 'post',
+			'supports' => [ 'title', 'editor' ],
+		] );
+	}
+
+	public function log_query( $query ) {
+		// If we are already logging, do not log again
+		if ( $this->is_logging || ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) || wp_doing_cron() ) {
+			return $query;
+		}
+
+		// Log only INSERT and UPDATE queries
+		if ( ! preg_match( '/^\s*(INSERT|UPDATE)\s/i', $query ) ) return $query;
+		if ( strpos( $query, 'wp_options' ) !== false ) return $query;
+		if ( strpos( $query, 'auto-draft' ) !== false ) return $query;
+
+		$this->is_logging = true;
+		wp_insert_post( [
+			'post_type'    => 'sql_log',
+			'post_title'   => current_time( 'mysql' ),
+			'post_content' => $query,
+			'post_status'  => 'publish',
+		] );
+		$this->is_logging = false;
+
+		return $query;
+	}
+
+	public function clear_sql_logs() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			wp_die( 'Permission denied' );
+		}
+
+		$args = array(
+			'post_type' => 'sql_log',
+			'posts_per_page' => -1
+		);
+
+		$query = new WP_Query( $args );
+
+		while ( $query->have_posts() ) {
+			$query->the_post();
+			wp_delete_post( get_the_ID(), true );
+		}
+
+		wp_reset_postdata();
+
+		wp_redirect( admin_url( 'admin.php?page=sql-query-logger' ) );
+		exit;
+	}
+
 }
 
 new BlueprintGenerator();
